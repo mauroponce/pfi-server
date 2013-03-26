2416a7547c588585ed48da73283fcdb8b9be4426
Santiago Moral <santaigomoral@gmail.com>
2013-03-26 12:50:52 -0300

    Added user 131900 (smoral) to the database and to the recognition test

--- src/java/mauroponce/pfi/tests/RecognitionTest.java
+++ src/java/mauroponce/pfi/tests/RecognitionTest.java
@@ -1,14 +1,15 @@
 package mauroponce.pfi.tests;
-import mauroponce.pfi.recognition.FaceRecognizer;
+
+import mauroponce.pfi.recognition.*;
 
 public class RecognitionTest {
-	static String testImagePath = "C:/Users/Mauro/Documents/PFI/test/moralito.jpg";
+//	static String testImagePath = "C:/Users/Mauro/Documents/PFI/test/moralito.jpg";
+	static String testImagePath = "C:/Users/smoral/Documents/PFI/test/moralito.jpg";
 	
 	public static void main(String[] args) {
-		int k = 3;//number of nearest neighbors
+		int k = 4;//number of nearest neighbors
 		FaceRecognizer recognitionService = new FaceRecognizer();
-		String [] imagesFolders = {"131445", "131900", "131455"};
-		imagesFolders[0] = String.valueOf(131445);
+		String [] imagesFolders = {"131445", "131455", "131431", "131900"};
 		recognitionService.learn(imagesFolders);
 		recognitionService.recognize(testImagePath, k);
 	}	
